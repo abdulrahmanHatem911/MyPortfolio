704d1972cf8f74f44f6ed78ae0cede2f9123fd9e
ali hatem <AliHatem>
2022-10-06 14:42:50 +0200

    update header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -21,10 +21,8 @@ const Header = () => {
     <>
       <div className="header">
         <div className="row">
-          <div className="col-lg-6 col-md-6 col-sm-12 position-relative">
-            <div className="header-img ">
-              <img src={aboutmeData.heaaderimag} alt="placeholder" />
-            </div>
+          <div className="col-lg-6 col-md-6 col-sm-12 ">
+            <img src={aboutmeData.heaaderimag} alt="placeholder" />
           </div>
 
           <div class=" col-lg-6 col-md-6 col-sm-12">
